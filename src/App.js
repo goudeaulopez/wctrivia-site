8ffb4e7e526eb8d33778274260a95620c4cd1d28
leoGoudeau <goudeaulopez@gmail.com>
2025-07-12 17:36:19 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import fondo from "./assets/fondo.webp"
+import prese from "./assets/presentacion.webp"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="app-background"
+      style={{
+        backgroundImage: `url(${fondo})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+     <img src={prese} alt="Imagen centrada" style={{ maxWidth: "80%",height:"auto"}}/>
     </div>
   );
 }
